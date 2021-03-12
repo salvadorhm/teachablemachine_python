cb883e1042851a37187cdece9fe878544ff8219f
Salvador HM <salvadorhm@gmail.com>
2021-03-12 12:23:49 -0600

    Authentication with user/pass or TOKEN

--- api.py
+++ api.py
@@ -16,35 +16,40 @@ class Api():
     def POST(self):
         try:
             env = web.ctx.env
-            print(env)
+            # print(env)
             auth = env.get("HTTP_AUTHORIZATION")
-            print(auth)
-            # u,p =base64.decode("dXNlcjpwYXNz")
-            u= base64.b64decode("dXNlcjpwYXNz").decode("utf-8", "ignore")
-            print(u)
-            # print(p)
 
-            # if auth == "1234":
-            #     form = web.input(image={})
-            #     result ={}
-            #     filedir = 'downloads' # change this to the directory you want to store the file in.
-            #     if 'image' in form: # to check if the file-object is created
-            #         filepath = form.image.filename.replace('\\','/') # replaces the windows-style slashes with linux ones.
-            #         filename = filepath.split('/')[-1] # splits the and chooses the last part (the filename with extension)
-            #         # fout = open( filename,'wb') # creates the file where the uploaded file should be stored
-            #         fout = open(filedir +'/'+ filename,'wb') # creates the file where the uploaded file should be stored
-            #         fout.write(form.image.file.read()) # writes the uploaded file to the newly created file.
-            #         fout.close() # closes the file, upload complete.
-            #     filename = filepath.split('/')[-1]
-            #     result = classify.machineLearning(filedir +'/'+ filename)
-            #     web.webapi.header('Content-Type', 'application/json', unique=True)
-            #     web.webapi.OK(data='OK', headers={})
-            #     return json.dumps(result)
-            # else:
-            #     result = {}
-            #     result["status"] = 400
-            #     result["message"] = "token not found"
-            #     return json.dumps(result)
+            '''
+            Validate user and password
+            '''
+            auth = auth.replace("Basic ","")
+            u = base64.b64decode(auth).decode("utf-8", "ignore")
+            user,password = u.split(":")
+            print(user)
+            print(password)
+
+            if (user,password) in users:
+            # if auth == "1234": # Validate TOKEN
+                form = web.input(image={})
+                result ={}
+                filedir = 'downloads' # change this to the directory you want to store the file in.
+                if 'image' in form: # to check if the file-object is created
+                    filepath = form.image.filename.replace('\\','/') # replaces the windows-style slashes with linux ones.
+                    filename = filepath.split('/')[-1] # splits the and chooses the last part (the filename with extension)
+                    # fout = open( filename,'wb') # creates the file where the uploaded file should be stored
+                    fout = open(filedir +'/'+ filename,'wb') # creates the file where the uploaded file should be stored
+                    fout.write(form.image.file.read()) # writes the uploaded file to the newly created file.
+                    fout.close() # closes the file, upload complete.
+                filename = filepath.split('/')[-1]
+                result = classify.machineLearning(filedir +'/'+ filename)
+                web.webapi.header('Content-Type', 'application/json', unique=True)
+                web.webapi.OK(data='OK', headers={})
+                return json.dumps(result)
+            else:
+                result = {}
+                result["status"] = 400
+                result["message"] = "Authentication error"
+                return json.dumps(result)
         except Exception as error:
             result ={}
             result["status"] = "400"
--- request.py
+++ request.py
@@ -3,10 +3,17 @@ from requests.auth import HTTPBasicAuth
 
 url = 'http://localhost:8080'
 payload = {"image": open('keyboard2.jpg', 'rb')}
-# headers = {"Authorization": "1234"}
+
+'''
+Authorization with user and pass
+'''
 auth = HTTPBasicAuth("user","pass")
 headers = {"Authorization": auth}
-# response = requests.post(url, files=payload, auth = auth)
-response = requests.post(url, files=payload, auth = ("user","pass"))
+response = requests.post(url, files=payload, auth = auth)
+
+'''
+Authorization with a token
+'''
+# headers = {"Authorization": "1234"}
 # response = requests.post(url, files=payload, headers=headers)
 print(response.text)
